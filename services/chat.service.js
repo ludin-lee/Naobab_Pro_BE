81d95fceedff9c9de1f55851eda9fc978bc828ab
ludin-lee <alsdyd0331@naver.com>
2023-02-06 10:17:03 +0900

    :sparkles: 소켓기능 기능 변경

--- services/chat.service.js
+++ services/chat.service.js
@@ -12,12 +12,14 @@ class ChatService {
   //대화내용 호출
   showChat = async (diaryId, userId) => {
     const room = await this.diaryRepository.exDiary(diaryId);
-
+    const audienceId = room.userId === userId ? room.invitedId : room.userId;
     if (!room) throw new ValidationError('존재하지 않는 대화창입니다.');
     if (room.userId !== userId && room.invitedId !== userId)
       throw new AuthorizationError('권한이 없습니다');
 
-    return await this.chatRepository.showChat(diaryId);
+    const chatInfo = await this.chatRepository.showChat(diaryId);
+    chatInfo.audienceId = audienceId;
+    return chatInfo;
   };
 }
 
--- socket.js
+++ socket.js
@@ -57,34 +57,42 @@ const io = require('socket.io')(server, {
 io.on('connection', (socket) => {
   // 채팅 받아 저장 후, 전달
   socket.on('chat_message', async (data) => {
-    let { message, roomId, userId } = data;
-    socket.join(`room:${roomId}`);
+    let { message, diaryId, userId } = data;
+    socket.join(diaryId);
+
     const today = dayjs().tz().format('YYYY-MM-DD 00:00:00');
     const chatTime = new Date(today).setHours(new Date(today).getHours() - 9);
-
+    console.log(data);
     const [caseOne, caseTwo] = await Chats.findOrCreate({
-      where: { diaryId: roomId, createdAt: { [Op.gt]: chatTime } },
+      where: { diaryId, createdAt: { [Op.gt]: chatTime } },
       defaults: {
-        diaryId: roomId,
+        diaryId,
         userId: ADMIN, //관리자 키
         chat: `${dayjs(today).format('YYYY년 MM월 DD일')}`,
       },
     });
-    if (caseTwo) io.to(`room:${roomId}`).emit(today);
+    if (caseTwo) io.to(diaryId).emit(today);
 
     //채팅 내용 저장
-    await Chats.create({ roomId, userId, chat: message });
+    await Chats.create({ diaryId, userId, chat: message });
 
     const userInfo = await Users.findOne({ where: { userId } });
+    const diary = await Diaries.findOne({ where: { diaryId } });
+
+    const audienceId = diary.userId === userId ? diary.invitedId : diary.userId;
+    const audienceUserInfo = await Users.findOne({ where: { audienceId } });
     // 채팅 보내주기
     let newMessage = {
-      roomId,
+      diaryId,
+      userId,
       message,
       profileImg: userInfo.profileImg,
       nickname: userInfo.nickname,
+      audienceId,
+      audienceProfileImg: audienceUserInfo.profileImg,
       time: dayjs(new Date()).format(),
     };
-    socket.to(`room:${roomId}`).emit(newMessage); //채팅시 채팅방에 알림 생기게 프론트에서 소켓 콜백함수를 조정해야함
+    io.to(diaryId).emit('newMessage', newMessage); //채팅시 채팅방에 알림 생기게 프>론트에서 소켓 콜백함수를 조정해야함
   });
 
   // 초대 알림
@@ -100,11 +108,12 @@ io.on('connection', (socket) => {
     초대 수락: 4
     */
 
-    const diary = await Diaries.findOne({ where: { diaryId } }); //해당 다이어리 정보 조회
-
+    const diary = await Diaries.findOne({ where: { diaryId } }); //해당 다이어리정보 조회
+    socket.join(diaryId);
     // if (diary.invitedId) io.to(`canNotInvite`).emit(diary.diaryName);
-    if (diary.invitedId) socket.to(diaryId).emit('canNotInvite', 'false');
-    //다이어리에 이미 초대된 사람 있으면 해당 이벤트로 다이어리 이름 과 함께소켓 발생
+    if (diary.invitedId)
+      socket.to(diaryId).emit(`invite:${invitedUserId}`, false);
+    //다이어리에 이미 초대된 사람 있으면 해당 이벤트로 다이어리 이름 과 함께소켓발생
     else {
       //아니면 원래대로
       await Diaries.update(
@@ -129,7 +138,10 @@ io.on('connection', (socket) => {
           confirm: false,
         },
       });
-      socket.to(`invite:${invitedUserId}`).emit(caseTwo || caseOne, 'true');
+
+      socket
+        .to(diaryId)
+        .emit(`invite:${invitedUserId}`, true, caseTwo || caseOne);
     }
   });
 
@@ -149,7 +161,7 @@ io.on('connection', (socket) => {
       where: { notificationId: notification.notificationId },
     });
 
-    io.to(`inviteAccept:${notification.notificationId}`).emit(notification);
+    socket.to(notification.notificationId).emit(notification);
   });
 });
 
